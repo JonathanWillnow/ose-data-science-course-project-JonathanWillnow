496fd8809ab5bdee9054864d4df181e08a27ac35
JonathanWillnow <82315811+JonathanWillnow@users.noreply.github.com>
2021-07-03 17:17:00 +0200

    Delete example_project_auxiliary_tables.py
    leftover from template

--- auxiliary/example_project_auxiliary_tables.py
+++
@@ -1,9 +0,0 @@
-"""This module contains auxiliary functions for the creation of tables in the main notebook."""
-
-import json
-
-import matplotlib as plt
-import pandas as pd
-import numpy as np
-import statsmodels as sm
-
